c96abf7ae2d864897eacacf3fd7039a891561ee6
kamos1 <keji.amos@gmail.com>
2017-05-08 14:58:09 -0600

    create peopleCleaner function

--- src/helpers/peopleCleaner.js
+++ src/helpers/peopleCleaner.js
@@ -1,8 +1,15 @@
-import data from './stub/people.json';
+import data from '../stub/people.json';
 
-const peopleCleaner = (peopleObj) => {
-  
-
+const peopleCleaner = (data) => {
+  const array = data.results;
+  // console.log(array);
+  const people =  array.reduce((acc, person) => {
+    if(!acc[person.name]) {
+      acc[person.name] = {homeworld: person.homeworld, species: person.species}
+    }
+    return acc;
+  }, {})
+  return people;
 }
 
 export default peopleCleaner;
