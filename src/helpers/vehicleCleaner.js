b0bbb30fe4419839d5009f9d2e1f88222b421c45
Kelly Miller <kellyannemiller@gmail.com>
2017-05-08 19:09:35 -0600

    add vehicle and planet cleaner funcs and tests

--- src/helpers/vehicleCleaner.js
+++ src/helpers/vehicleCleaner.js
@@ -1,7 +1,15 @@
-import data from './stub/vehicle.json';
+import data from '../stub/vehicles.json';
 
-const vehicleCleaner = (vehicleObj) => {
-
+const vehicleCleaner = (data) => {
+    const array = data.results;
+    // console.log(array);
+    const vehicles =  array.reduce((acc, vehicle) => {
+      if(!acc[vehicle.name]) {
+        acc[vehicle.name] = {model: vehicle.model, class: vehicle.vehicle_class, passengers: vehicle.passengers}
+      }
+      return acc;
+    }, {})
+    return vehicles;
 }
 
 export default vehicleCleaner;
