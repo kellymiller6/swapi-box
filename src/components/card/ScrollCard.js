97e3718775c3eff6b85c75bd6a8bd7d77de2cca0
kamos1 <keji.amos@gmail.com>
2017-05-10 18:11:40 -0600

    scroll card test

--- src/components/card/ScrollCard.js
+++ src/components/card/ScrollCard.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const ScrollCard = ({quote}) => {
   const display = quote[Math.floor(Math.random() * quote.length)]
  return(
-   <div className='card'>
+   <div className='card scroll-card'>
     {display}
    </div>
 )
