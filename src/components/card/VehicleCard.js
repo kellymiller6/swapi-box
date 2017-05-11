4fb1acd98dbef6f81b431053c7237ff837b5b049
kamos1 <keji.amos@gmail.com>
2017-05-10 18:07:31 -0600

    vehicle card tests

--- src/components/card/VehicleCard.js
+++ src/components/card/VehicleCard.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const VehicleCard = ({vehicleObject, model, car_class, passengers, handleFavorite}) => {
  return(
-   <div className='card'>
+   <div className='card vehicle-card'>
      <h3>{model}</h3>
      <p>{car_class}</p>
      <p>{passengers}</p>
